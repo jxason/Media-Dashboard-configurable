92d598dec216b93dfdc7b902f7ac744bc34f4602
Sebastian <Sebawtisn@gmail.com>
2025-07-23 18:49:49 -0600

    Start Over, API 10%

--- MediaDashboardPro/MediaDashboardPro/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace MediaDashboardPro.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
