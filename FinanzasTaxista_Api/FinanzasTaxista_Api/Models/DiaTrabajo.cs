0190cc3f77593d0c32af15a0d6361b8bb804ffa8
J4so0n <jasonalisol1610@gmail.com>
2025-07-27 21:44:46 -0600

    Sp_dia trabajo creado con exito

--- FinanzasTaxista_Api/FinanzasTaxista_Api/Models/DiaTrabajo.cs
+++ FinanzasTaxista_Api/FinanzasTaxista_Api/Models/DiaTrabajo.cs
@@ -5,8 +5,14 @@ namespace FinanzasTaxista_Api.Models
     public class DiaTrabajo
     {
         [Key]
-        public int id { get; set; }
-        public DateOnly fecha { get; set; }
-    
+        public int Id { get; set; }
+
+        [Required]
+        public int IdUsuario { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime Fecha { get; set; }
+      
     }
 }
