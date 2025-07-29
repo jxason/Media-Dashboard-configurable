6b72aa1d957636af1f080a32a42ca82c50f39f19
PUDaniel14 <danielurbinaCODE@gmail.com>
2025-07-28 22:44:43 -0600

    Arreglo registro.

--- FinanzasTaxista_View/FinanzasTaxista_View/Pages/Account/Registro.cshtml.cs
+++ FinanzasTaxista_View/FinanzasTaxista_View/Pages/Account/Registro.cshtml.cs
@@ -33,7 +33,7 @@ namespace FinanzasTaxista_View.Pages.Views.Account
                 if (response)
                 {
                     message = "Usuario registrado correctamente.";
-                    return RedirectToPage("/Views/Account/Login");
+                    return RedirectToPage("/Account/Login");
                 }
                 else
                 {
