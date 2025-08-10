fa5a4c7468e3b38b072a44dce5f5713b703f055c
Steven <schacon40980@ufide.ac.cr>
2025-08-10 15:13:56 -0600

    Modificación en las vistas

--- FinanzasTaxista_View/FinanzasTaxista_View/Models/CategoriaModel.cs
+++ FinanzasTaxista_View/FinanzasTaxista_View/Models/CategoriaModel.cs
@@ -1,9 +1,12 @@
-﻿namespace FinanzasTaxista_View.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FinanzasTaxista_View.Models
 {
     public class CategoriaModel
     {
         public int id { get; set; }
 
+        [Required(ErrorMessage = "El nombre de la categoria es obligatorio.")]
         public string nombre_categoria { get; set; } = string.Empty;
     
     }
